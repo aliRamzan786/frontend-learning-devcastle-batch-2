3feb5b6a4f118be60c3b485412c9d95555ba19ee
Ameer Hamza <142672783+AmeerHamza6758@users.noreply.github.com>
2024-07-26 10:23:34 -0700

    JS started

---
+++ javaScript/dataTypes/dataType.js
@@ -0,0 +1,45 @@
+console.log("JavaScript file is connected and working!");
+
+//Number
+let x = 12.4;
+let y = 15;
+// console.log(`The number of ${x} and ${y}`);
+
+//string
+
+let stringData = "Hello";
+let stringData2 = "World";
+let stringData3 = `Hello, World`;
+
+// console.log(
+//   `String concatenation: ${stringData} and ${stringData2} and ${stringData3}`
+// );
+
+//Boolean
+let isLoggedIn = false;
+let isLoggedOut = true;
+console.log(typeof(isLoggedIn), isLoggedOut);
+
+//undefined
+let myName = undefined
+console.log(myName)
+myName = "John Doe"
+console.log(myName)
+
+//Null
+let z = null;
+console.log(z)
+
+//non-premitive
+let isArray = [10,12,5,7,22,'John Doe', 55]
+// console.log(isArray)
+
+//Object
+let americanPerson ={
+    myName:'John Doe',
+    class:"intermediate",
+    contact:992266
+}
+
+console.log(americanPerson)
+
